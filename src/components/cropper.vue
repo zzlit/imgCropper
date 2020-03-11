d4513f51547c85214f75fd697f9e36a9ba14fe13
zhouxy <874331618@qq.com>
2020-03-11 11:10:20 +0800

    上传功能

---
+++ src/components/cropper.vue
@@ -0,0 +1,162 @@
+<template>
+  <div>
+    <div class="img">
+      <div class="img-box">
+        <img v-if="imgurl" ref="image" alt="crop" :src="imgurl" width="200px">
+      </div>
+      <div class="preview-box">
+        <div class="big">
+          <img v-if="preview" :src="preview" alt="big preview">
+        </div>
+        <div class="normal">
+          <img v-if="preview" :src="preview" alt="normal preview">
+        </div>
+        <div class="small">
+          <img v-if="preview" :src="preview" alt="small preview">
+        </div>
+      </div>
+    </div>
+    <div class="operate">
+      <button @click="onChoose">选择图片</button>
+      <button @click="onDownload">下载裁剪图片</button>
+    </div>
+    <input style="display: none;" type="file" ref="uploads" accept="image/png, image/jpeg, image/gif, image/jpg" @change="onUploadImg($event)">
+
+  </div>
+</template>
+
+<script>
+import Cropper from 'cropperjs'
+import 'cropperjs/dist/cropper.css'
+
+export default {
+  name: 'Cropper',
+  data() {
+    return {
+      options: {
+        viewMode: 1,
+        initialAspectRatio: 1,
+        movable: false,
+        zoomable: false,
+        rotatable: false,
+        scalable: false,
+        guides: false,
+        cropBoxResizable: false
+      },
+      cropper: null,
+      imgurl: '',
+      preview: '',
+      isPreview: true
+    }
+  },
+  methods: {
+    init() {
+      this.$nextTick(() => {
+        const options = {
+          crop: () => {
+            this.onPreview()
+          }
+        }
+        this.cropper = new Cropper(this.$refs.image, Object.assign(options, this.options))
+      })
+    },
+    onPreview() {
+      if (this.isPreview) {
+        this.isPreview = false
+        setTimeout(() => {
+          this.isPreview = true
+        }, 50)
+      } else {
+        return false
+      }
+      const canvas = this.cropper.getCroppedCanvas()
+      this.preview = canvas.toDataURL()
+    },
+    onUploadImg(e) {
+      const file = e.target.files[0]
+      if (!/\.(gif|jpg|jpeg|png|bmp|GIF|JPG|PNG)$/.test(e.target.value)) {
+        alert('图片类型必须是.gif,jpeg,jpg,png,bmp中的一种')
+        return false
+      }
+      const reader = new FileReader()
+      reader.onload = e => {
+        if (typeof e.target.result === "object") {
+          this.imgurl = window.URL.createObjectURL(new Blob([e.target.result]))
+        } else {
+          this.imgurl = e.target.result
+        }
+        this.init()
+      }
+      reader.readAsArrayBuffer(file)
+    },
+    onDownload() {
+      const a = document.createElement('a')
+      a.download = 'demo'
+      a.href = this.preview
+      document.body.appendChild(a)
+      a.click()
+      document.body.removeChild(a)
+    },
+    onChoose() {
+      this.$refs.uploads.click()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.img {
+  display: flex;
+  justify-content: center;
+  .img-box {
+    width: 400px;
+    height: 400px;
+    &:empty {
+      background-color: rgb(247, 247, 247);
+    }
+    img {
+      display: block;
+      max-width: 100%;
+      margin: 0 auto;
+    }
+  }
+  .preview-box {
+    margin-left: 40px;
+    .big, .normal, .small {
+      &:empty {
+        background-color: rgb(247, 247, 247);
+      }
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .big {
+      width: 200px;
+      height: 200px;
+      margin-bottom: 25px;
+    }
+    .normal {
+      width: 100px;
+      height: 100px;
+      margin-bottom: 25px;
+    }
+    .small {
+      width: 50px;
+      height: 50px;
+    }
+  }
+}
+.operate {
+  margin-top: 15px;
+  button {
+    border-radius: 4px;
+    padding: 2px 12px;
+    outline: none;
+  }
+  button+button {
+    margin-left: 20px;
+  }
+}
+</style>
---
+++ src/components/upload.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="upload">
+    <div :id="id" style="width: 100%; height: 100%;" />
+    <img class="upload-img" ref="uploadimg" :src="imgurl" alt="upload" v-if="imgurl">
+  </div>
+</template>
+
+<script>
+import plupload from 'plupload'
+
+export default {
+  name: 'Upload',
+  props: {
+    id: {
+      type: String,
+      default: 'UPLOAD'
+    }
+  },
+  data () {
+    return {
+      alioss: {},
+      options: {
+        runtimes: 'html5, flash, silverlight, html4',
+        multi_selection: false,
+        flash_swf_url: '/plupload/js/Moxie.swf',
+        silverlight_xap_url: '/plupload/js/Moxie.xap'
+      },
+      plupload: null,
+      imgurl: '', // 预览的图片地址 base64 or blob
+      uploadImgurl: ''
+    }
+  },
+  methods: {
+    getOssConfig () {
+      const url = /* aliossapi + */ 'app/cdn/token?dir=admin/icon_img'
+      this.$ajax.get(url).then(({data: {data, code}}) => {
+        /*
+        * 处理相关操作
+        */
+        this.alioss = data
+        this.initFrontUpload()
+      })
+    },
+    initFrontUpload () {
+      const options = Object.assign(
+        {
+          browse_button: this.id,
+          url: this.alioss.host,
+          init: {
+            BeforeUpload: this.beforeUpload,
+            FilesAdded: this.filesAdded,
+            FileUploaded: this.fileUploaded
+          }
+        },
+        this.options
+      )
+      this.plupload = new plupload.Uploader(options)
+      this.plupload.init()
+    },
+    beforeUpload (up, files) {
+      this.plupload.setOption({
+        multipart_params: {
+          key: this.alioss.dir + '/' + this.uploadImgurl,
+          policy: this.alioss.policy,
+          OSSAccessKeyId: this.alioss.accessid,
+          success_action_status: 200,
+          signature: this.alioss.signature
+        }
+      })
+    },
+    filesAdded (up, files) {
+      const moxie = plupload.moxie
+      const file = files[0]
+
+      const suffix = files[0].name.split('.').pop() // 文件类型
+      const timeStamp = new Date().getTime() // 时间戳
+      const random = Math.floor(Math.random() * Math.floor(10000000)) // 随机数
+      this.uploadImgurl = `${timeStamp}_${random}.${suffix}`
+      if (file.type === 'image/gif') {
+        let fr = new moxie.file.FileReader()
+        fr.onload = () => {
+          this.imgurl = fr.result
+          fr.destroy()
+          fr = null
+        }
+        fr.readAsDataURL(file.getSource())
+      } else {
+        let preview = new moxie.image.Image()
+        preview.onload = () => {
+          preview.resize({
+            width: 100,
+            fit: false
+          })
+          preview.crop(100, 100)
+          this.imgurl = preview.type === 'image/jpeg' ? preview.getAsDataURL('image/jpeg', 80) : preview.getAsDataURL()
+          preview.destroy()
+          preview = null
+        }
+        preview.load(file.getSource())
+      }
+    },
+    fileUploaded (up, file, response) {
+      const image = new Image()
+      image.src = this.alioss.dir + '/' + this.uploadImgurl
+      image.onload = () => {
+        console.log(image.width, image.height)
+      }
+      // this.$emit('success', response.status === 200 ? this.uploadImgurl : null)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.upload {
+  position: relative;
+  width: 100px;
+  height: 100px;
+  background-color: #ececec;
+  cursor: pointer;
+}
+.upload-img {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
